19080658918514b2f9ca796b739ed4e2db10f67a
sttk <sttk.xslet@gmail.com>
2023-03-26 00:33:24 +0900

    update: removed libarg (moved to github.com/sttk-go/cliargs)

--- libarg/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright (C) 2023 Takayuki Sato. All Rights Reserved.
-// This program is free software under MIT License.
-// See the file LICENSE in this distribution for more details.
-
-/*
-Package github.com/sttk-go/clidax/libarg is a library to parse command line arguments for Golang application.
-*/
-package libarg
